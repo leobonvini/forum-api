4297d25971b36f3f81fdfd0e8db31b8113c317d1
Leonardo Bonvini <leo.bonvini@greenant.com.br>
2022-07-28 09:52:54 -0300

    Create topic model

--- src/main/kotlin/br/com/alura/forum/model/Topic.kt
+++ src/main/kotlin/br/com/alura/forum/model/Topic.kt
@@ -1,5 +1,14 @@
 package br.com.alura.forum.model
 
+import java.time.LocalDateTime
+
 data class Topic(
-    val id: Long
+    val id: Long? = null,
+    val title: String,
+    val message: String,
+    val creationDate: LocalDateTime = LocalDateTime.now(),
+    val course: Course,
+    val author: User,
+    val status: StatusTopic = StatusTopic.NOT_RESPONDED
+
 )
